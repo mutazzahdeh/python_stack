0d87f06cc2c3df337de8170ae7f002ed1b471f3e
mutazzahdeh <mutazzahdeh@gmail.com>
2021-05-13 18:50:30 +0300

    flask

---
+++ Flask/Checkerboard/checker.py
@@ -0,0 +1,11 @@
+from flask import Flask,render_template
+app=Flask(__name__)
+
+@app.route("/")
+@app.route("/<int:x>/<int:y>/")
+@app.route("/<int:x>/<int:y>/<color1>/<color2>")
+def name(x=8,y=8,color1="red",color2="black"):
+    return render_template("index.html",x=x,y=y,color1=color1,color2=color2)
+
+if __name__=="__main__":
+    app.run(debug=True)
