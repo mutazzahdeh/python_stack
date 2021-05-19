542fca9fb4c549aa3be408290b89f6dd284e0ae6
mutazzahdeh <mutazzahdeh@gmail.com>
2021-05-19 20:37:21 +0300

    django

--- django/django_fundamentals/Great_Number_Game/Great_Number/urls.py
+++ django/django_fundamentals/Great_Number_Game/Great_Number/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path 
 from . import views
 urlpatterns=[
-    path("",views.aaa)
+    path("",views.index),
+    path("submet",views.num),
+    path("playagin",views.plyagine),
 ]
--- django/django_fundamentals/Great_Number_Game/Great_Number/views.py
+++ django/django_fundamentals/Great_Number_Game/Great_Number/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render,HttpResponse,redirect
-
+import random 	
 # Create your views here.
-def aaa(reqest):
-    return HttpResponse("asdsadsf")
+def index(request):
+    
+    if not("rand" in request.session) :
+        request.session["rand"]=random.randint(1, 100)
+        print(request.session["rand"])
+    return render(request,"index.html")
+
+def num(request):
+    request.session["num"]=int(request.POST["number1"])
+    request.session["bool"]=True
+    return redirect("/")
+
+
+
+def plyagine(request):
+    request.session["rand"]=random.randint(1, 100)
+    print(request.session["rand"])
+    request.session["bool"]=False
+    return redirect("/")
