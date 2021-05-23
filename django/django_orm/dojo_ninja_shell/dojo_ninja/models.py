ecdac2ebd40060922c971decf3653f01ca6356d2
mutazzahdeh <mutazzahdeh@gmail.com>
2021-05-23 15:02:37 +0300

    python

---
+++ django/django_orm/dojo_ninja_shell/dojo_ninja/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class dojos(models.Model):
+    name = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    desc = models.CharField(max_length=255)
+  
+class ninjas(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    dojo = models.ForeignKey(dojos, related_name="dojo", on_delete = models.CASCADE)
+    
