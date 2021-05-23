05c5c475ec7a5e3266a89d344bb6cec8b351da95
mutazzahdeh <mutazzahdeh@gmail.com>
2021-05-23 08:28:51 +0300

    django

--- django/django_orm/users_with_template/user_app/models.py
+++ django/django_orm/users_with_template/user_app/models.py
@@ -3,7 +3,7 @@ from django.db import models
     
 class users(models.Model):
     first_name = models.CharField(max_length=255)
-    Email =models.CharField(max_length=255)
     Last_name = models.CharField(max_length=255)
+    Email =models.CharField(max_length=255)
     age = models.IntegerField()
  
--- django/django_orm/users_with_template/user_app/views.py
+++ django/django_orm/users_with_template/user_app/views.py
@@ -5,6 +5,18 @@ from .models import users
 # Create your views here.
 def index(request):
     context = {
-    	"all_the_movies": Movie.objects.all()
+    	"users": users.objects.all()
     }
-    return render(request,"index.html")
+    return render(request,"index.html",context)
+
+def add(request):
+ 
+
+    if request.method == "POST":
+        users.objects.create(
+    	first_name = request.POST['fname'],
+    	Last_name = request.POST['lname'],
+    	Email = request.POST['email'],
+    	age = request.POST['age'],)
+     
+    return redirect("/")
