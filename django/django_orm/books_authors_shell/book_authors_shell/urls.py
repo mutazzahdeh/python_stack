f24293b9ad66bbe54a783627f4736ea4125b3e89
mutazzahdeh <mutazzahdeh@gmail.com>
2021-05-25 00:30:58 +0300

    django

--- django/django_orm/books_authors_shell/book_authors_shell/urls.py
+++ django/django_orm/books_authors_shell/book_authors_shell/urls.py
@@ -2,4 +2,8 @@ from django.urls import path,include
 from . import views
 urlpatterns = [
     path('', views.index),
+    path('addbook', views.addbook),
+    path('books/<int:number>', views.bookdesc),
+    path('addaut', views.addaut),
 ]
+
--- django/django_orm/books_authors_shell/book_authors_shell/views.py
+++ django/django_orm/books_authors_shell/book_authors_shell/views.py
@@ -1,5 +1,45 @@
 from django.shortcuts import render,HttpResponse,redirect
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse("jfljdsdslfmwqelkm")
+    context={
+        "book":books.objects.all()
+    }
+    return render(request,"index.html",context)
+
+def addbook(request):
+    if request.method == "POST":
+        
+        books.objects.create(
+        title  = request.POST['name'],
+    	desc  = request.POST['desc'],
+    	)
+        return redirect("/")
+
+def bookdesc(request,number):
+    
+    x=books.objects.get(id=number)
+    y=x.author.all()
+    request.session["id"]=number
+
+    context={
+        "book":books.objects.get(id=number),
+        'aut':y,
+        'authour':authors.objects.all()
+        
+    }
+
+    return render(request,"index2.html",context)
+
+def addaut(request):
+    if request.method=='POST':
+        x=books.objects.get(id=request.session["id"])
+        z=authors.objects.get(id=request.POST["author"])
+        y=x.author.add(z)
+        return redirect(f'books/{request.session["id"]}')
+
+
+    
+    
+
